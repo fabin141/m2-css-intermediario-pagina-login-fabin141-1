510cc390dfd9bbf6a05be16d4e7f8745fe1451b8
Fabio <fabiowillian.shr@gmail.com>
2022-09-23 16:43:20 -0300

    Finalizando Projeto

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,20 @@
-/* Desenvolva seu código aqui... */
+let button          = document.querySelector(".esqueciSenha")
+let background      = document.querySelector(".back")
+let closeX          = document.querySelector(".btnOut")
+let push            = document.querySelector(".send")
+
+button.addEventListener("click",function(event){
+event.preventDefault()
+background.classList.add("aparecerTelaPorCima")
+})
+
+
+closeX.addEventListener("click",function(event){
+event.preventDefault()
+background.classList.remove("aparecerTelaPorCima")
+})
+
+push.addEventListener("click",function(event){
+event.preventDefault()
+background.classList.remove("aparecerTelaPorCima")
+})
